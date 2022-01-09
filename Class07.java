4b3ec230755c8001c257259a526cef4e58ebf9c0
wbhongggg <92263847+wbhongggg@users.noreply.github.com>
2022-01-09 10:10:45 +0800

    Create Class07.java

---
+++ Class07.java
@@ -0,0 +1,21 @@
+package Java14;
+import java.io.*;
+public class hw14_7
+{
+   public static void main(String args[]) throws IOException
+   {
+      int count=0;
+      String str;
+      FileReader fr=new FileReader("c:\\java\\donkey.txt");
+      BufferedReader bfr=new BufferedReader(fr);
+
+      while((str=bfr.readLine())!=null)
+      {
+         if(count==1)
+            bfr.skip(14);
+         System.out.println(str);
+         count++;
+      }
+      fr.close();
+   }
+}
