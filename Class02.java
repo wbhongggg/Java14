01d0d73f269f12613b57fc582c9e3abde0581d86
wbhongggg <92263847+wbhongggg@users.noreply.github.com>
2022-01-09 10:06:59 +0800

    Create Class02.java

---
+++ Class02.java
@@ -0,0 +1,16 @@
+package Java14;
+import java.util.Scanner;
+import java.lang.String;
+public class hw14_2
+{
+   public static void main(String args[])
+   {
+      Scanner scn=new Scanner(System.in);
+      String str;
+
+      System.out.print("��J�r��:");
+      str=scn.nextLine();
+
+      System.out.println("�ഫ���j�g: "+str.toUpperCase());
+   }
+}
